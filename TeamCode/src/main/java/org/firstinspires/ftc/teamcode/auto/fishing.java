c3298d560a12a8af1ca7895ab30364c4c8f3b92d
micahlegomyeggo <micahlegomyeggo@gmail.com>
2024-11-16 18:12:47 -0700

    Preliminary Auto - Don't touch these

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/fishing.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+public class fishing {
+
+    //this program is preliminary code for a two arm auto
+
+    //setup (camera too)
+
+    //camera looks for clumps of blue or red samples for highest chance of getting one first try
+
+        //if biggest clump is on the other half go to next biggest clump
+
+    //main loop
+
+        //camera looks for clumps of blue or red samples for highest chance of getting one first try
+
+        //extend arm once sample has been removed
+
+        //color sensor looks for blue or red sample below grabber
+
+        //if one isn't directly below keep looping (while)
+
+        //grab sample once confirmed and robot does not have another
+
+        //return arm
+
+        //notify hanging
+
+        //immediately start looking for another sample
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/hanging.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+public class hanging {
+
+    //this is preliminary code for a two arm auto
+
+    //setup
+
+    //main loop
+
+        //wait for notification of sample from fishing
+
+        //grab sample out of other arm
+
+        //clip the sample
+
+        //extend and rotate arm (at same time to be more efficient) to the high bar
+
+        //reset clipping mechanism somewhere in here
+
+        //clip sample onto high bar
+
+        //return arm to normal
+
+}
